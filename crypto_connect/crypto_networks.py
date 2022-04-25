8028ad0dacb0345f70b908a65a6a8be85c13835e
Paulius11 <paulius.gaizauskas@gmail.com>
2022-04-25 21:15:46 +0300

    Refactor code

--- crypto_connect/crypto_networks.py
+++ crypto_connect/crypto_networks.py
@@ -1,5 +1,6 @@
 import json
 from decimal import Decimal
+from enum import Enum
 
 import requests
 from dotenv import dotenv_values
@@ -41,7 +42,7 @@ BSC_TESTNET_FACTORY = config_network.get("PANKAKE_FACTORY_V2_TESTNET")
 BSC_TESTNET_ROUTER = config_network.get("PANKAKE_ROUTER_V2_TESTNET")
 #API
 BSC_TESTNET_BLOCK_EXPLORER_API_URL = config_network.get("BSC_TESTNET_BLOCK_EXPLORER_API")
-
+BSC_TESTNET_BLOCK_EXPLORER_API_KEY = config_private.get("BSC_TESTNET_BLOCK_EXPLORER_API_KEY")
 
 
 NETWORKS = config_network
@@ -59,6 +60,9 @@ ABI_FACTORY_PAIR = json.loads(
     '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"sync","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
 )
 
+class Network(Enum):
+    BSC_MAINNET = 56
+    BSC_TESTNET = 97
 
 def check_address(contract_address):
     """Checks address"""
@@ -69,9 +73,11 @@ class CryptoNetworkConnector:
     """
     Automatically connect to testnet or mainet
     """
-    def __init__(self, testnet: bool):
-        self.testnet = testnet
-        if testnet:
+    def __init__(self, network: Network):
+        self.selected_network = network
+
+        if network == Network.BSC_TESTNET:
+            messege = "Running BSC testnet..."
             self.network = BSC_TESTNET
             self.wbnb: ChecksumAddress = check_address(BSC_TESTNET_WBMB)
             self.busd: ChecksumAddress = check_address(BSC_TESTNET_BUSD)
@@ -80,9 +86,11 @@ class CryptoNetworkConnector:
             self.gas = '10'
             self.chain = int(BSC_TESTNET_CHAIN_ID)
             self.explorer_api_url = BSC_TESTNET_BLOCK_EXPLORER_API_URL
+            self.explorer_url = "https://testnet.bscscan.com/"
+            self.automate_market_maker = "https://pancake.kiemtienonline360.com/"
 
-        else:
-            # mainet
+        if network == Network.BSC_MAINNET:
+            messege = "Running BSC mainnet..."
             self.network = BSC_MAINNET
             self.wbnb: ChecksumAddress = check_address(BSC_MAINNET_WBMB)
             self.busd: ChecksumAddress = check_address(BSC_MAINNET_BUSD)
@@ -92,11 +100,10 @@ class CryptoNetworkConnector:
             self.chain = int(BSC_MAINNET_CHAIN_ID)
             self.explorer_api_url = BSC_MAINNET_EXPLORER_API_URL
             self.explorer_api_key = BSC_MAINNET_EXPLORER_API_KEY
+            self.explorer_url = "https://bscscan.com/"
+            self.automate_market_maker = "https://pancakeswap.finance/"
 
-        if testnet:
-            print("Running BSC testnet...")
-        else:
-            print("Running BSC mainnet...")
+        print(messege)
         print(f'Network  : {self.network}')
         print(f'WBNB     : {self.wbnb}')
         print(f'BUST     : {self.busd}')
@@ -105,6 +112,8 @@ class CryptoNetworkConnector:
         print(f'GAS      : {self.gas}')
         print(f'CHAIN ID : {self.chain}')
         print(f'API      : {self.explorer_api_url}')
+        print(f'EXPLORER : {self.explorer_url}')
+        print(f'AMM      : {self.automate_market_maker}')
 
         self.web3 = self.init_web3()
         print(f'Connected: {self.web3.isConnected()}')
@@ -151,7 +160,7 @@ class CryptoNetworkConnector:
 
         return Decimal('0')
 
-    def get_price_bnb(self, custom_token, base_currency):
+    def get_price_in_dollars(self, custom_token, base_currency):
         """
         Get price in usdt
         :param custom_token: token you want to check
@@ -161,6 +170,8 @@ class CryptoNetworkConnector:
         custom_token_checked = check_address(custom_token)
 
         pair = self._get_pair_address(contract_address=custom_token, base_currency=base_currency)
+        if pair == '0x0000000000000000000000000000000000000000':
+            return
         pair_contract = self.web3.eth.contract(address=pair, abi=ABI_FACTORY_PAIR)
         is_reversed = pair_contract.functions.token0().call().lower() == base_currency.lower()
         decimals, token_name, token_symbol = self.get_token_data(custom_token_checked)
@@ -209,47 +220,53 @@ class CryptoNetworkConnector:
 
     def get_bnb_price(self):
         try:
-            bnbPrice = requests.get(
-                self.explorer_api_url + "/api?module=stats&action=bnbprice&apikey=" + self.explorer_api_key)
-            price = bnbPrice.json()['result']['ethusd']
-            print(f"BNB: {price} USD")
+            if self.selected_network == Network.BSC_MAINNET:
+                bnbPrice = requests.get(
+                    self.explorer_api_url + "/api?module=stats&action=bnbprice&apikey=" + self.explorer_api_key)
+                price = bnbPrice.json()['result']['ethusd']
+                print(f"BNB: {price} USD")
+            if self.selected_network == Network.BSC_TESTNET:
+                print("Skipping BNB check....")
+                return
+
         except TypeError:
            raise "Please add 'BSC_MAINNET_EXPLORER_API_KEY' api in .evn "
         return price
 
     def print_transaction_link(self, transaction_hash):
         base_url = "https://bscscan.com/"
-        if self.testnet:
+        if self.selected_network == Network.BSC_TESTNET:
             base_url = "https://testnet.bscscan.com/"
         print(f'Transaction: {base_url}/tx/{transaction_hash}')
 
     def print_detected_transaction(self, address):
         base_url = "https://bscscan.com"
-        if self.testnet:
+        if self.selected_network == Network.BSC_TESTNET:
             base_url = "https://testnet.bscscan.com"
         print(f'Address: {base_url}/address/{address}')
         print(f'Token  : {base_url}/token/{address}')
 
     def print_pair(self, pair_tx):
         base_url = "https://bscscan.com"
-        if self.testnet:
+        if self.selected_network == Network.BSC_TESTNET:
             base_url = "https://testnet.bscscan.com"
         print(f'Pair: {base_url}/address/{pair_tx}')
 
     def print_verify_link(self, created_contract_address):
         base_url = "https://bscscan.com"
-        if self.testnet:
+        if self.selected_network == Network.BSC_TESTNET:
             base_url = "https://testnet.bscscan.com"
         print(f'Verify@: {base_url}/verifyContract?a={created_contract_address}')
 
 
 def main():
-    n = CryptoNetworkConnector(testnet=False)
+    n = CryptoNetworkConnector(Network.BSC_TESTNET)
     # n.get_abi("0x2859e4544c4bb03966803b044a93563bd2d0dd4d")  # Mainnet
     n.get_bnb_price()
-    # fak = n.get_factory()
-    fak = n._get_pair_address("0x6e0bef56b648b3eebae3808b8cbe7efe8755ad9c", base_currency=n.wbnb)
-    price = n.get_price_bnb("0x6e0bef56b648b3eebae3808b8cbe7efe8755ad9c", base_currency=n.wbnb)
+    fak = n.get_factory()
+    pari_address = n._get_pair_address("0xc2A2d9D339d70343e068E2D7414D3361093C06e5", base_currency=n.wbnb)
+    # price = n.get_price_in_dollars("0x6e0bef56b648b3eebae3808b8cbe7efe8755ad9c", base_currency=n.wbnb) #Mainet
+    price = n.get_price_in_dollars("0xA5F1b78C3909e8Ff66CE939dc9357008c9344633", base_currency=n.wbnb) #Testnet
 
 
 if __name__ == "__main__":
